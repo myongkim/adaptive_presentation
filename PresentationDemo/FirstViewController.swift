a7577a500f5fd90a169da7a0160f3968d0f49644
Isaac Kim <myong328@gmail.com>
2017-11-06 15:18:21 -0800

    Initial Commit

---
+++ PresentationDemo/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  PresentationDemo
+//
+//  Created by echessa on 2/23/16.
+//  Copyright © 2016 Career Foundry. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlertButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ PresentationDemo/SecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SecondViewController.swift
+//  PresentationDemo
+//
+//  Created by echessa on 2/23/16.
+//  Copyright © 2016 Career Foundry. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func actionButtonTapped(_ sender: UIBarButtonItem) {
+    }
+
+
+}
+
